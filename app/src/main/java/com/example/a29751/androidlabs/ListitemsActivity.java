b61f587683a26707838ff6c8889628a9a3a408eb
29751 <dll891234>
2017-09-19 18:53:25 -0400

    Added 3 activities for Lab2

--- app/src/main/java/com/example/a29751/androidlabs/ListitemsActivity.java
+++ app/src/main/java/com/example/a29751/androidlabs/ListitemsActivity.java
@@ -3,12 +3,11 @@ package com.example.a29751.androidlabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListitemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_listitems);
     }
 }
